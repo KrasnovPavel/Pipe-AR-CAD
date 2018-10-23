f2bd94ef6eb5f5ad4aead963170acf9cc24ae7c4
Pavel Krasnov <krasnovpavel0@gmail.com>
2018-10-23 16:14:23 +0300

    New rendering for direct tubes

--- HoloApp/Assets/Materials/Shaders/Wireframe.shader
+++ HoloApp/Assets/Materials/Shaders/Wireframe.shader
@@ -1,15 +1,9 @@
-﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
-// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
-
-Shader "Grid" {
+﻿Shader "Grid" {
 	Properties{
 	  _GridThickness("Grid Thickness", Float) = 0.01
-	  _Length("Length", Float) = -1
-	  _Offset("Offset", Float) = 0.025
-	  _GridSpacingXZ("Grid Spacing XZ", Float) = 1.0
 	  _GridSpacingY("Grid Spacing Y", Float) = 1.0
-	  _GridColour("Grid Colour", Color) = (0.5, 1.0, 1.0, 1.0)
-	  _BaseColour("Base Colour", Color) = (0.0, 0.0, 0.0, 0.0)
+	  _GridColor("Grid Color", Color) = (0.5, 1.0, 1.0, 1.0)
+	  _BaseColor("Base Color", Color) = (0.0, 0.0, 0.0, 0.0)
 	}
 	SubShader{
 		Tags { "Queue" = "Transparent" }
@@ -25,10 +19,9 @@ Shader "Grid" {
 			// Access Shaderlab properties
 			uniform float _GridThickness;
 			uniform float _GridSpacingY;
-			uniform float _Offset;
 			uniform float _Length;
-			uniform float4 _GridColour;
-			uniform float4 _BaseColour;
+			uniform float4 _GridColor;
+			uniform float4 _BaseColor;
 
 			// Input into the vertex shader
 			struct vertexInput {
@@ -40,6 +33,7 @@ Shader "Grid" {
 				float4 pos : SV_POSITION;
 				float4 worldPos : TEXCOORD0;
 				float3 localPos: POSITION1;
+				float3 worldScale: SCALE;
 			};
 
 			// VERTEX SHADER
@@ -48,20 +42,23 @@ Shader "Grid" {
 				output.pos = UnityObjectToClipPos(input.vertex);
 				output.worldPos = mul(unity_ObjectToWorld, input.vertex);
 				output.localPos = input.vertex.xyz;
+				output.worldScale = float3(
+					length(float3(unity_ObjectToWorld[0].x, unity_ObjectToWorld[1].x, unity_ObjectToWorld[2].x)), // scale x axis
+					length(float3(unity_ObjectToWorld[0].y, unity_ObjectToWorld[1].y, unity_ObjectToWorld[2].y)), // scale y axis
+					length(float3(unity_ObjectToWorld[0].z, unity_ObjectToWorld[1].z, unity_ObjectToWorld[2].z))  // scale z axis
+				);
 				return output;
 			}
 
 			// FRAGMENT SHADER
 			float4 frag(vertexOutput input) : COLOR {
-				if (frac(input.worldPos.y / _GridSpacingY) < _GridThickness * 4 ||
+				if (frac(input.localPos.y * input.worldScale.y / input.worldScale.z) < _GridThickness ||
 					(input.localPos.x < _GridThickness && input.localPos.x > -_GridThickness) ||
-					(input.localPos.z < _GridThickness && input.localPos.z > -_GridThickness) ||
-					(input.localPos.y < 0.003f) ||
-					(_Length > 0 && input.localPos.y > _Length - 0.003f))
+					(input.localPos.z < _GridThickness && input.localPos.z > -_GridThickness))
 				{
-					return _GridColour;
+					return _GridColor;
 				}
-				return _BaseColour;
+				return _BaseColor;
 			}
 			ENDCG
 		   }
--- HoloApp/Assets/Scripts/Tube.cs
+++ HoloApp/Assets/Scripts/Tube.cs
@@ -1,115 +1,54 @@
-﻿using System;
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.Receivers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tube : MonoBehaviour {
-    protected List<Line> lines;
-    protected List<GameObject> circles;
-    // values in meters
+public class Tube : InteractionReceiver {
+    GameObject tube;
+    GameObject startPoint;
+    GameObject endPoint;
+
     [Range(0.01f, 1f)]
-    public float outerDiameter;
-    [Range(0f, 10f)]
-    public float length;
-    [Range(0.001f, 0.01f)]
-    public float lineWidth = 0.001f;
-    public Material redMaterial;
-    public Material yellowMaterial;
-
-    public bool isFirstPivotUsed = false;
-    public bool isSecondPivotUsed = false;
-    protected GameObject startLight;
-    protected GameObject endLight;
-
-    public const int numberOfLines = 8;
+    public float diameter = 0.01f;
+    [Range(0.1f, 10f)]
+    public float length = 0.1f;
 
 	// Use this for initialization
-	protected void Start ()
+	void Start ()
     {
-        startLight = transform.Find("StartPoint").gameObject;
-        endLight = transform.Find("EndPoint").gameObject;
-        startLight.GetComponent<Circle>().material = yellowMaterial;
-        endLight.GetComponent<Circle>().material = yellowMaterial;
-
-        lines = new List<Line>();
-        circles = new List<GameObject>();
-        MakeLines(1, redMaterial);
-        CalculateLine(0, 0, 0);
-        MakeLines(numberOfLines, yellowMaterial);
-        CalculateLine(outerDiameter, 1, numberOfLines + 1);
+        tube = transform.Find("Tube").gameObject;
+        startPoint = transform.Find("Start Point").gameObject;
+        endPoint = transform.Find("End Point").gameObject;
     }
-    
-    void Update()
+	
+	// Update is called once per frame
+	void Update ()
     {
-        CalculateLine(0, 0, 0);
-        CalculateLine(outerDiameter, 1, numberOfLines + 1);
-
-        startLight.SetActive(!isFirstPivotUsed);
-        endLight.SetActive(!isSecondPivotUsed);
-        startLight.GetComponent<Light>().range = outerDiameter / 2;
-        endLight.GetComponent<Light>().range = outerDiameter / 2;
-        endLight.transform.localPosition = new Vector3(0, 0, length);
-
-        CalculateCircles();
+        tube.transform.localScale = new Vector3(diameter, length, diameter);
+        endPoint.transform.localPosition = new Vector3(0, 0, length);
+        endPoint.GetComponent<Light>().range = diameter;
+        startPoint.GetComponent<Light>().range = diameter;
     }
 
-    protected void MakeLines(int numberOfLines, Material material)
+    protected override void InputDown(GameObject obj, InputEventData eventData)
     {
-        for (int i = 0; i < numberOfLines; ++i)
+        switch (obj.name)
         {
-            var go = new GameObject("line");
-            go.transform.SetParent(gameObject.transform);
-            go.transform.localPosition = Vector3.zero;
-            lines.Add(go.AddComponent<Line>());
-            lines[lines.Count-1].material = material;
-        }
-    }
-
-    protected void CalculateLine(float diameter, int startLine, int endLine)
-    {
-        Quaternion quat = Quaternion.Euler(0, 0, 360f / numberOfLines);
-        Vector3 startPoint = new Vector3(0, diameter / 2, 0);
-        for (int i = startLine; i <= endLine && i < lines.Count; ++i)
-        {
-            lines[i].length = length;
-            startPoint = quat * startPoint;
-            lines[i].startPosition = startPoint;
-            lines[i].width = lineWidth;
-        }
-    }
-
-    protected void CalculateCircles()
-    {
-        startLight.GetComponent<Circle>().diameter = outerDiameter;
-        startLight.GetComponent<Circle>().width = lineWidth;
-        endLight.GetComponent<Circle>().diameter = outerDiameter;
-        endLight.GetComponent<Circle>().width = lineWidth;
-
-        int numberOfCircles = 0;
-        float circlesInterval = outerDiameter * 2;
-        if (outerDiameter > 0)
-        {
-            numberOfCircles = Mathf.CeilToInt(length / circlesInterval);
-        }
-
-        for (int i = circles.Count; i < numberOfCircles; ++i)
-        {
-            GameObject circle = new GameObject("circle");
-            circle.transform.SetParent(gameObject.transform);
-            circle.AddComponent<Circle>().material = yellowMaterial;
-            circles.Add(circle);
-        }
-        for (int i = circles.Count - 1; i > numberOfCircles - 1; --i)
-        {
-            Destroy(circles[i]);
-            circles.RemoveAt(i);
-        }
-        for (int i = 0; i < circles.Count; ++i)
-        {
-            circles[i].transform.localPosition = new Vector3(0, 0, i * circlesInterval);
-            circles[i].transform.localRotation = Quaternion.identity;
-            circles[i].GetComponent<Circle>().diameter = outerDiameter;
-            circles[i].GetComponent<Circle>().width = lineWidth;
+            case "IncreaseLengthButton":
+                length += 0.3f;
+                break;
+            case "DecreaseLengthButton":
+                length -= 0.3f;
+                break;
+            case "IncreaseDiameterButton":
+                diameter += 0.1f;
+                break;
+            case "DecreaseDiameterButton":
+                diameter -= 0.1f;
+                break;
+            default:
+                break;
         }
     }
 }
