01d57020ba85338bd07c746cf4402fe4d78147b3
Pavel Krasnov <krasnovpavel0@gmail.com>
2018-11-12 18:18:06 +0300

    Working v0.1

--- HoloApp/Assets/Scripts/BendedTube.cs
+++ HoloApp/Assets/Scripts/BendedTube.cs
@@ -3,64 +3,63 @@ using HoloToolkit.Unity.Receivers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BendedTube : InteractionReceiver
 {
-    List<Mesh> meshes;
-    GameObject tube;
-    GameObject startPoint;
-    GameObject endPoint;
-    public GameObject buttonBar;
+    private List<Mesh> _meshes;
+    private GameObject _tube;
+    private GameObject _endPoint;
+    public GameObject ButtonBar;
 
-    bool _useSecondRadius;
-    int _angle;
+    private bool _useSecondRadius;
+    private int _angle;
 
     [Range(0.035f, 1.2f)]
-    public float firstBendRadius = 0.07f;
+    public float FirstBendRadius = 0.07f;
     [Range(0.055f, 0.87f)]
-    public float secondBendRadius = 0.055f;
+    public float SecondBendRadius = 0.055f;
     [Range(0.014f, 0.426f)]
-    public float diameter = 0.025f;
-    public Material material;
+    public float Diameter = 0.025f;
+    public Material Material;
 
-    public int angle
+    public int Angle
     {
         get { return _angle; }
         set
         {
-            if (value < MeshFactory.deltaAngle || value > 180)
+            if (value < MeshFactory.DeltaAngle || value > 180)
             {
                 return;
             }
             _angle = value;
 
-            setMesh();
+            SetMesh();
         }
     }
 
-    public bool useSecondRadius
+    public bool UseSecondRadius
     {
         get { return _useSecondRadius; }
         set
         {
             _useSecondRadius = value;
 
-            setMesh();
+            SetMesh();
         }
     }
 
     // Use this for initialization
     void Start ()
     {
-        meshes = MeshFactory.CreateMeshes(diameter, firstBendRadius, secondBendRadius);
-        tube = transform.Find("Tube").gameObject;
-        startPoint = transform.Find("Start Point").gameObject;
-        endPoint = transform.Find("End Point").gameObject;
+        _meshes = MeshFactory.CreateMeshes(Diameter, FirstBendRadius, SecondBendRadius);
+        _tube = transform.Find("Tube").gameObject;
+        _endPoint = transform.Find("End Point").gameObject;
         _useSecondRadius = false;
-        _angle = MeshFactory.deltaAngle;
-        buttonBar.GetComponent<ButtonBar>().offset = 0.7f * diameter;
-        tube.GetComponent<MeshRenderer>().material.SetFloat("_Diameter", diameter);
-        setMesh();
+        _angle = MeshFactory.DeltaAngle;
+        ButtonBar.GetComponent<ButtonBar>().Offset = 0.7f * Diameter;
+        _tube.GetComponent<MeshRenderer>().material.SetFloat("_Diameter", Diameter);
+        SetMesh();
     }
 
     protected override void InputDown(GameObject obj, InputEventData eventData)
@@ -68,34 +67,38 @@ public class BendedTube : InteractionReceiver
         switch (obj.name)
         {
             case "IncreaseAngleButton":
-                angle += MeshFactory.deltaAngle;
+                Angle += MeshFactory.DeltaAngle;
                 break;
             case "DecreaseAngleButton":
-                angle -= MeshFactory.deltaAngle;
+                Angle -= MeshFactory.DeltaAngle;
                 break;
             case "ClockwiseButton":
-                transform.localRotation *= Quaternion.Euler(0, 0, MeshFactory.deltaAngle);
+                transform.localRotation *= Quaternion.Euler(0, 0, MeshFactory.DeltaAngle);
                 break;
             case "AnticlockwiseButton":
-                transform.localRotation *= Quaternion.Euler(0, 0, -MeshFactory.deltaAngle);
+                transform.localRotation *= Quaternion.Euler(0, 0, -MeshFactory.DeltaAngle);
                 break;
             case "ChangeRadiusButton":
-                useSecondRadius = !useSecondRadius;
+                UseSecondRadius = !UseSecondRadius;
                 break;
-            default:
+            case "AddBendButton":
+                gameObject.GetComponent<TubeFactory>().CreateBendedTube(_endPoint.transform, Diameter);
+                break;
+            case "AddTubeButton":
+                gameObject.GetComponent<TubeFactory>().CreateTube(_endPoint.transform, Diameter);
                 break;
         }
     }
 
-    void setMesh()
+    private void SetMesh()
     {
-        tube.transform.localPosition = new Vector3(_useSecondRadius ? -secondBendRadius : -firstBendRadius, 0, 0);
+        _tube.transform.localPosition = new Vector3(_useSecondRadius ? -SecondBendRadius : -FirstBendRadius, 0, 0);
 
-        Quaternion rot = Quaternion.Euler(0, -angle, 0);
-        Vector3 pos = new Vector3(_useSecondRadius ? secondBendRadius : firstBendRadius, 0, 0);
-        endPoint.transform.localPosition = rot * pos - pos;
-        endPoint.transform.localRotation = rot;
-        int numberOfAngles = 180 / MeshFactory.deltaAngle;
-        tube.GetComponent<MeshFilter>().mesh = meshes[_angle / MeshFactory.deltaAngle - 1 + (useSecondRadius ? numberOfAngles : 0)];
+        Quaternion rot = Quaternion.Euler(0, -Angle, 0);
+        Vector3 pos = new Vector3(_useSecondRadius ? SecondBendRadius : FirstBendRadius, 0, 0);
+        _endPoint.transform.localPosition = rot * pos - pos;
+        _endPoint.transform.localRotation = rot;
+        int numberOfAngles = 180 / MeshFactory.DeltaAngle;
+        _tube.GetComponent<MeshFilter>().mesh = _meshes[_angle / MeshFactory.DeltaAngle - 1 + (UseSecondRadius ? numberOfAngles : 0)];
     }
 }
---
+++ HoloApp/Assets/Scripts/TubeStart.cs
@@ -0,0 +1,58 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.Receivers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubeStart : InteractionReceiver
+{
+    private GameObject _tube;
+    private GameObject _endPoint;
+
+    private const float Length = 0.03f;
+
+    private float _diameter = 0.014f;
+
+    public float Diameter
+    {
+        get { return _diameter; }
+        set
+        {
+            if (value <= 0)
+            {
+                return;
+            }
+            _diameter = value;
+            Debug.Log(_diameter);
+            _tube.transform.localScale = new Vector3(_diameter, Length, _diameter);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        _tube = transform.Find("Tube").gameObject;
+        _endPoint = transform.Find("End Point").gameObject;
+        _endPoint.transform.localPosition = new Vector3(0, 0, Length);
+        Diameter = 0.05f;
+    }
+
+    protected override void InputDown(GameObject obj, InputEventData eventData)
+    {
+        switch (obj.name)
+        {
+            case "IncreaseDiameterButton":
+                Diameter += 0.01f;
+                break;
+            case "DecreaseDiameterButton":
+                Diameter -= 0.01f;
+                break;
+            case "AddBendButton":
+                gameObject.GetComponent<TubeFactory>().CreateBendedTube(_endPoint.transform, Diameter);
+                break;
+            case "AddTubeButton":
+                gameObject.GetComponent<TubeFactory>().CreateTube(_endPoint.transform, Diameter);
+                break;
+        }
+    }
+}
