234695c95e9fc6dd411f4606677b6a83b25ad47a
BillDoorAndBinky <DYROGONICHE.doc@gmail.com>
2019-07-03 10:49:37 +0300

    Тест позиционирования по меткам

---
+++ HoloApp/Assets/HoloCAD/Scripts/MarkTrackableEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class MarkTrackableEventHandler : DefaultTrackableEventHandler
+{
+
+   public int Id;
+
+   protected override void OnTrackingFound()
+   {
+      base.OnTrackingFound();
+      // table add mark
+      GameObject TableObject = GameObject.Find("Table");
+      MarksHandler TableObjectMarksHandler =  TableObject.GetComponent<MarksHandler>();
+      TableObjectMarksHandler.ActiveMarks.Add(gameObject);
+   }
+   protected override void OnTrackingLost()
+   {
+       base.OnTrackingLost();
+       // table remove mark
+      
+       GameObject TableObject = GameObject.Find("Table");
+       MarksHandler TableObjectMarksHandler =  TableObject.GetComponent<MarksHandler>();
+       TableObjectMarksHandler.ActiveMarks.Remove(gameObject);
+   }
+
+}
---
+++ HoloApp/Assets/HoloCAD/Scripts/MarksHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity;
+using UnityEngine;
+using Vuforia;
+public class MarksHandler : MonoBehaviour
+{
+    public GameObject[] AllMarks;
+    public List<GameObject> ActiveMarks = new List<GameObject>();
+    public Vector3[] PositionsOfMarks;
+    public Vector3[] RotationsOfMarks;
+    public Vector3 PrevRotation;
+    public Vector3 PrevCenterVector;
+    public Vector3 PrevPos;
+    private void Update()
+    {
+        if (ActiveMarks.Count == 0)
+        {
+            if (PrevPos != null)
+            {
+               // Quaternion RotationOfVector = Quaternion.Euler(PrevRotation.x,PrevRotation.y,PrevRotation.z);
+               // Vector3 VectorBetweenCenterAndMark = RotationOfVector * new Vector3(PrevCenterVector.x,PrevCenterVector.y,PrevCenterVector.z);
+                transform.position = new Vector3(PrevPos.x-PrevCenterVector.x,PrevPos.y-PrevCenterVector.y,PrevPos.z-PrevCenterVector.z);
+                transform.rotation = Quaternion.Euler(PrevRotation.x, PrevRotation.y,PrevRotation.z);
+            }
+           
+        }
+        else
+        {
+            var rendererComponents = GetComponentsInChildren<Renderer>(true);
+            foreach (var component in rendererComponents)
+                component.enabled = true;
+            //TODO: научиться считать средний угол поворота меток через задание каждой метке вектора наклона
+            PrevRotation= new Vector3(ActiveMarks[0].transform.rotation.x - RotationsOfMarks[0].x, 
+                ActiveMarks[0].transform.rotation.y- RotationsOfMarks[0].y,
+                ActiveMarks[0].transform.rotation.z- RotationsOfMarks[0].z);
+            PrevPos = new Vector3(0,0,0);
+            PrevCenterVector = new Vector3(0,0,0);
+            foreach (GameObject activeMark in ActiveMarks)
+            {
+                
+                PrevCenterVector+=PositionsOfMarks[activeMark.GetComponent<MarkTrackableEventHandler>().Id] / ActiveMarks.Count;
+                PrevPos += activeMark.transform.position / ActiveMarks.Count;
+                Quaternion RotationOfVector = Quaternion.Euler(activeMark.transform.eulerAngles.x,activeMark.transform.eulerAngles.y,activeMark.transform.eulerAngles.z);
+                Vector3 VectorBetweenCenterAndMark = RotationOfVector * PositionsOfMarks[activeMark.GetComponent<MarkTrackableEventHandler>().Id];
+                PrevCenterVector += VectorBetweenCenterAndMark/ ActiveMarks.Count;
+            }
+            transform.position = new Vector3(PrevPos.x-PrevCenterVector.x,PrevPos.y-PrevCenterVector.y,PrevPos.z-PrevCenterVector.z);
+            transform.rotation = Quaternion.Euler(PrevRotation.x, PrevRotation.y,PrevRotation.z);
+            Debug.Log(String.Format("{0},{1},{2}",PrevRotation.x,PrevPos.x,PrevCenterVector.x));
+        }
+        
+        
+    }
+}
