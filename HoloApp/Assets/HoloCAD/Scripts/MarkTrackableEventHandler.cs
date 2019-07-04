3d461dd117fe7df5dc89cc4016a1c85b915af656
BillDoorAndBinky <DYROGONICHE.doc@gmail.com>
2019-07-04 12:59:19 +0300

    Добавлена привязка объекта к нескольким меткам

--- HoloApp/Assets/HoloCAD/Scripts/MarkTrackableEventHandler.cs
+++ HoloApp/Assets/HoloCAD/Scripts/MarkTrackableEventHandler.cs
@@ -5,23 +5,34 @@ public class MarkTrackableEventHandler : DefaultTrackableEventHandler
 {
 
    public int Id;
+   public bool IsActive;
+   public override void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+   {
+       base.OnTrackableStateChanged(previousStatus, newStatus);
+       if (newStatus == TrackableBehaviour.Status.TRACKED) OnTrackingFoundMark();
+       if (newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) OnTrackingLostMark();
+       Debug.Log($"Status changed: {previousStatus} {newStatus}");
+   }
 
-   protected override void OnTrackingFound()
+   protected  void OnTrackingFoundMark()
    {
       base.OnTrackingFound();
       // table add mark
+      
+      Debug.Log($"Target {Id} found");
       GameObject TableObject = GameObject.Find("Table");
       MarksHandler TableObjectMarksHandler =  TableObject.GetComponent<MarksHandler>();
-      TableObjectMarksHandler.ActiveMarks.Add(gameObject);
+      IsActive = true;
    }
-   protected override void OnTrackingLost()
+   protected  void OnTrackingLostMark()
    {
        base.OnTrackingLost();
        // table remove mark
-      
+       
+       Debug.Log($"Target {Id} lost");
        GameObject TableObject = GameObject.Find("Table");
        MarksHandler TableObjectMarksHandler =  TableObject.GetComponent<MarksHandler>();
-       TableObjectMarksHandler.ActiveMarks.Remove(gameObject);
+       IsActive = false;
    }
 
 }
--- HoloApp/Assets/HoloCAD/Scripts/MarksHandler.cs
+++ HoloApp/Assets/HoloCAD/Scripts/MarksHandler.cs
@@ -7,50 +7,44 @@ using Vuforia;
 public class MarksHandler : MonoBehaviour
 {
     public GameObject[] AllMarks;
-    public List<GameObject> ActiveMarks = new List<GameObject>();
     public Vector3[] PositionsOfMarks;
     public Vector3[] RotationsOfMarks;
     public Vector3 PrevRotation;
-    public Vector3 PrevCenterVector;
     public Vector3 PrevPos;
     private void Update()
     {
-        if (ActiveMarks.Count == 0)
+        foreach (GameObject currentMark in AllMarks)
         {
-            if (PrevPos != null)
-            {
-               // Quaternion RotationOfVector = Quaternion.Euler(PrevRotation.x,PrevRotation.y,PrevRotation.z);
-               // Vector3 VectorBetweenCenterAndMark = RotationOfVector * new Vector3(PrevCenterVector.x,PrevCenterVector.y,PrevCenterVector.z);
-                transform.position = new Vector3(PrevPos.x-PrevCenterVector.x,PrevPos.y-PrevCenterVector.y,PrevPos.z-PrevCenterVector.z);
-                transform.rotation = Quaternion.Euler(PrevRotation.x, PrevRotation.y,PrevRotation.z);
-            }
-           
-        }
-        else
-        {
+            if (!currentMark.GetComponent<MarkTrackableEventHandler>().IsActive) continue;
             var rendererComponents = GetComponentsInChildren<Renderer>(true);
             foreach (var component in rendererComponents)
                 component.enabled = true;
-            //TODO: научиться считать средний угол поворота меток через задание каждой метке вектора наклона
-            PrevRotation= new Vector3(ActiveMarks[0].transform.rotation.x - RotationsOfMarks[0].x, 
-                ActiveMarks[0].transform.rotation.y- RotationsOfMarks[0].y,
-                ActiveMarks[0].transform.rotation.z- RotationsOfMarks[0].z);
-            PrevPos = new Vector3(0,0,0);
-            PrevCenterVector = new Vector3(0,0,0);
-            foreach (GameObject activeMark in ActiveMarks)
-            {
-                
-                PrevCenterVector+=PositionsOfMarks[activeMark.GetComponent<MarkTrackableEventHandler>().Id] / ActiveMarks.Count;
-                PrevPos += activeMark.transform.position / ActiveMarks.Count;
-                Quaternion RotationOfVector = Quaternion.Euler(activeMark.transform.eulerAngles.x,activeMark.transform.eulerAngles.y,activeMark.transform.eulerAngles.z);
-                Vector3 VectorBetweenCenterAndMark = RotationOfVector * PositionsOfMarks[activeMark.GetComponent<MarkTrackableEventHandler>().Id];
-                PrevCenterVector += VectorBetweenCenterAndMark/ ActiveMarks.Count;
-            }
-            transform.position = new Vector3(PrevPos.x-PrevCenterVector.x,PrevPos.y-PrevCenterVector.y,PrevPos.z-PrevCenterVector.z);
-            transform.rotation = Quaternion.Euler(PrevRotation.x, PrevRotation.y,PrevRotation.z);
-            Debug.Log(String.Format("{0},{1},{2}",PrevRotation.x,PrevPos.x,PrevCenterVector.x));
+            PrevRotation = new Vector3(
+                currentMark.transform.eulerAngles.x -
+                RotationsOfMarks[currentMark.GetComponent<MarkTrackableEventHandler>().Id].x,
+                currentMark.transform.eulerAngles.y -
+                RotationsOfMarks[currentMark.GetComponent<MarkTrackableEventHandler>().Id].y,
+                currentMark.transform.eulerAngles.z -
+                RotationsOfMarks[currentMark.GetComponent<MarkTrackableEventHandler>().Id].z);
+            Vector3 RotationOfMark = new Vector3(
+                currentMark.transform.eulerAngles.x -
+                RotationsOfMarks[currentMark.GetComponent<MarkTrackableEventHandler>().Id].x,
+                currentMark.transform.eulerAngles.y -
+                RotationsOfMarks[currentMark.GetComponent<MarkTrackableEventHandler>().Id].y,
+                currentMark.transform.eulerAngles.z -
+                RotationsOfMarks[currentMark.GetComponent<MarkTrackableEventHandler>().Id].z);
+            Vector3 VectorOfRotationCenter = Quaternion.Euler(RotationOfMark) *
+                                         PositionsOfMarks[currentMark.GetComponent<MarkTrackableEventHandler>().Id];
+            PrevPos = (currentMark.transform.position - VectorOfRotationCenter);
+            break;
         }
-        
-        
+
+        if (PrevPos != null)
+        {
+            transform.position = PrevPos;
+            transform.rotation = Quaternion.Euler(PrevRotation.x, PrevRotation.y, PrevRotation.z);
+        }
+
+
     }
 }
