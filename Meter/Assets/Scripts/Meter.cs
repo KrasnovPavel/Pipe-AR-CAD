6fa05da2e66e6727de03dba24997d7317670a0ca
Pavel Krasnov <krasnovpavel0@gmail.com>
2019-08-19 15:46:49 +0300

    Проект для измерения погрешностей

---
+++ Meter/Assets/Scripts/Meter.cs
@@ -0,0 +1,96 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using JetBrains.Annotations;
+using UnityEngine;
+
+#if ENABLE_WINMD_SUPPORT
+	using System;
+	using System.Collections.Generic;
+    using Windows.Storage;
+    using Windows.Storage.Pickers;
+#endif
+
+/// <summary> Класс, рассчитывающий и отображающий расстояние между соединяемыми трубами. </summary>
+[RequireComponent(typeof(LineRenderer))]
+ public sealed class Meter : MonoBehaviour
+{
+	// ReSharper disable once NotNullMemberIsNotInitialized
+	/// <summary> Первая метка. </summary>
+	[Tooltip("Первая метка.")]
+	[NotNull] public Transform First;
+	
+	// ReSharper disable once NotNullMemberIsNotInitialized
+	/// <summary> Вторая метка. </summary>
+	[Tooltip("Вторая метка.")]
+	[NotNull] public Transform Second;
+	
+	/// <summary> Расстояние между метками. </summary>
+	public float DeltaLength { get; private set; }
+	
+	/// <summary> Угол между метками. </summary>
+	public float DeltaAngle { get; private set; }
+	
+	public void ChooseFile()
+	{
+		UnityEngine.WSA.Application.InvokeOnUIThread(PickFile, true);
+	}
+
+	public void SaveCurrentData()
+	{
+#if ENABLE_WINMD_SUPPORT
+		FileIO.AppendTextAsync(_file, $"{DeltaLength:0.000}; {DeltaAngle:000.0};"); 
+#endif
+	}
+	
+	#region Unity event functions
+
+	/// <summary> Функция, инициализирующая объект в Unity. </summary>
+	private void Start()
+	{
+		_camera = Camera.main.transform;
+		_renderer = GetComponent<LineRenderer>();
+		_renderer.useWorldSpace = true;
+		_label = transform.Find("Label").GetComponent<TextMesh>();
+	}
+
+	private void Update()
+	{
+		Vector3 pos1 = First.position;
+		Vector3 pos2 = Second.position;
+		
+		DeltaLength = (pos1 - pos2).magnitude;
+		DeltaAngle = Vector3.Angle(First.forward, Second.forward);
+		_renderer.SetPositions(new [] { pos1, pos2 });
+		
+		_label.transform.LookAt(_camera);
+		_label.transform.Rotate(0, 180, 0);
+		_label.transform.position = pos1 + (pos2 - pos1) / 2;
+		_label.text = $"L: {DeltaLength:0.000}м, Δα: {DeltaAngle:000.0}°";
+	}
+
+	#endregion
+
+	#region Private definitions
+
+	private LineRenderer _renderer;
+	private TextMesh _label;
+	private Transform _camera;
+	
+#if ENABLE_WINMD_SUPPORT
+	private StorageFile _file;
+#endif
+
+	private async void PickFile()
+	{
+#if ENABLE_WINMD_SUPPORT
+		FileSavePicker savePicker = new FileSavePicker();
+		savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+		savePicker.FileTypeChoices.Add("Meter csv", new List<string>() { ".csv" });
+		_file = await savePicker.PickSaveFileAsync();
+		await FileIO.WriteTextAsync(_file, "L, м; Δα, °;\n"); 
+#endif
+	}
+
+	#endregion
+}
---
+++ Meter/Assets/Scripts/XboxInputManager.cs
@@ -0,0 +1,107 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+    /// <summary> Обработчик данных с xbox-геймпада. </summary>
+public class XboxInputManager : MonoBehaviour
+{
+    /// <summary> Время после которого зажатая кнопка считается повторно нажатой. </summary>
+    public float TimeThreshold = 0.3f;
+
+    /// <summary> Множитель показаний осей. </summary>
+    public float TimeFactor = 1;
+
+    //TODO: Механизм определения подключен ли геймпад.
+    public bool IsGamepadConnected = false;
+
+    public Meter meter;
+    
+    #region Unity event functioin
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+    
+    private void Update()
+    {
+        if (!IsGamepadConnected) return;
+        
+        CalculatePressingTime();
+        
+        CheckFireOnce("JoystickA", meter.SaveCurrentData);
+        CheckFireOnce("JoystickStart", meter.ChooseFile);
+    }
+
+    #endregion
+    
+    #region Private definitions
+
+    private Camera _camera;
+
+    private readonly Dictionary<string, float> _pressingTime = new Dictionary<string, float>();
+    private readonly HashSet<string> _fireOnce = new HashSet<string>();
+    
+    private void CalculatePressingTime()
+    {
+        string[] keys = new string[_pressingTime.Count];
+        _pressingTime.Keys.CopyTo(keys, 0);
+        foreach (string key in keys)
+        {
+            if (Input.GetAxis(key) < -0.3f || Input.GetAxis(key) > 0.3f)
+            {
+                _pressingTime[key] += Time.deltaTime;
+                if (_pressingTime[key] > TimeThreshold && !_fireOnce.Contains(key)) _pressingTime[key] = 0;
+            }
+            else
+            {
+                _pressingTime[key] = 0;
+            }
+        }
+    }
+    
+    private void CheckRepeatPressing(string buttonName, Action action)
+    {
+        if (!_pressingTime.ContainsKey(buttonName)) _pressingTime.Add(buttonName, 0);
+
+        if (Math.Abs(_pressingTime[buttonName] - Time.deltaTime) > float.Epsilon) return;
+        
+        if (Input.GetAxis(buttonName) > 0.3f) action();
+    }
+
+    private void CheckRepeatPressingAxis(string buttonName, Action actionPlus, Action actionMinus)
+    {
+        if (!_pressingTime.ContainsKey(buttonName)) _pressingTime.Add(buttonName, 0);
+
+        if (Math.Abs(_pressingTime[buttonName] - Time.deltaTime) > float.Epsilon) return;
+
+        if      (Input.GetAxis(buttonName) >  0.3f) actionPlus();
+        else if (Input.GetAxis(buttonName) < -0.3f) actionMinus();
+    }
+
+    private void CheckFireOnce(string buttonName, Action action)
+    {
+        if (!_fireOnce.Contains(buttonName))
+        {
+            _pressingTime.Add(buttonName, 0);
+            _fireOnce.Add(buttonName);
+        }
+
+        if (Math.Abs(_pressingTime[buttonName] - Time.deltaTime) > float.Epsilon) return;
+        
+        if (Input.GetAxis(buttonName) > 0.3f) action();
+    }
+
+    private void CheckAxisChange(string axisName, Action<float> action, float timeFactor = 1)
+    {
+        if (Input.GetAxis(axisName) < 0.3f || Input.GetAxis(axisName) > 0.3f)
+        {
+            action(Input.GetAxis(axisName) * Time.deltaTime * timeFactor);
+        }
+    }
+    
+    #endregion
+}
