aa8c255d4c52c71f97cd26faacfd2ee1400fd428
BillDoorAndBinky <Dyrogoniche.doc@gmail.com>
2019-11-22 13:24:16 +0300

    В редактор добавлен импорт из glTF в меши

---
+++ MarksEditor/Assets/Assets/Scripts/glTF/GlTFImporter.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using Boo.Lang;
+using HoloCore;
+using UnityEngine;
+using static SFB.StandaloneFileBrowser;
+namespace MarksEditor.glTF
+{
+    
+    public class GlTFImporter : Singleton<GlTFImporter>
+    {
+
+
+        public Material Mat;
+        private void Start()
+        {
+            ImportglTFFile();
+        }
+        
+        public void ImportglTFFile()
+        {
+            string filepath = OpenDialog();
+            string JSONString  = ReadJSONStringFromFile(filepath);
+            root rootOfglTFFile = GetRootFileFromJSONString(JSONString);
+            GetGameObjectsFromImportedglTFFile(rootOfglTFFile);
+        }
+
+        private string OpenDialog()
+        {
+            string[] paths = OpenFilePanel("Веберите glTF-файл","","gltf",false);
+            if (paths.Length == 0)
+            {
+                throw new Exception("Файл не выбран!");
+            }
+            
+            
+            return paths[0];
+        }
+        
+        private string ReadJSONStringFromFile(string path)
+        {
+            string jsonString;
+            StreamReader streamReader = new StreamReader(path);
+            
+            try
+            {
+                jsonString = streamReader.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+                throw;
+            }
+            
+            streamReader.Close();
+            return jsonString;
+        }
+
+        private root GetRootFileFromJSONString(string JSONString)
+        {
+            root glTFFileRoot; 
+            try
+            {
+               
+                glTFFileRoot = JsonUtility.FromJson<root>(JSONString);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+                throw;
+            }
+
+            return glTFFileRoot;
+        }
+        
+        private void GetGameObjectsFromImportedglTFFile(root rootOfglTFFile)
+        {
+            List<GameObject> gameObjectsList = new List<GameObject>();
+            int currentGameObjectIndex = 0;
+            Transform parentTransform = MarkPlaceController.Instance.Target.transform;
+            foreach (scene currentScene in rootOfglTFFile.scenes)
+            {
+                GameObject currentSceneGameObject = new GameObject(Convert.ToString(currentGameObjectIndex++));
+                currentSceneGameObject.transform.parent = parentTransform;
+                
+                foreach (int currentSceneNodeIndex in currentScene.nodes)
+                {
+                    node currentNode = rootOfglTFFile.nodes[currentSceneNodeIndex];
+                    GameObject currentNodeGameObject = new GameObject(currentNode.name);
+                    currentNodeGameObject.transform.parent = currentSceneGameObject.transform;
+                    FormMeshesFromglTF(currentNodeGameObject, currentNode, rootOfglTFFile);
+                }
+                
+            }
+        }
+
+        private void FormMeshesFromglTF(GameObject currentNodeGameObject, node currentNode, root rootOfglTFFile)
+        {
+                currentNodeGameObject.transform.localRotation = Quaternion.Euler((new Quaternion(currentNode.rotation[0],
+                currentNode.rotation[1], currentNode.rotation[2], currentNode.rotation[3])).eulerAngles);
+            currentNodeGameObject.transform.localPosition = new Vector3(currentNode.translation[0],currentNode.translation[1],currentNode.translation[2]);
+            currentNodeGameObject.transform.localScale = new Vector3(currentNode.scale[0],currentNode.scale[1],currentNode.scale[2]);
+            mesh currentMesh = rootOfglTFFile.meshes[currentNode.mesh];
+            foreach (primitive currentMeshPrimitive in currentMesh.primitives)
+            {
+                GameObject currentPrimitiveGameObject = new GameObject();
+                currentPrimitiveGameObject.transform.parent = currentNodeGameObject.transform;
+                currentPrimitiveGameObject.transform.localScale  = new Vector3(1,1,1);
+                currentPrimitiveGameObject.transform.localPosition  = new Vector3(0,0,0);
+                currentPrimitiveGameObject.transform.localRotation  = Quaternion.Euler(0,0,0);
+                MeshFilter currentMeshFilter = currentPrimitiveGameObject.AddComponent<MeshFilter>();
+                
+                MeshRenderer currentMeshRenderer = currentPrimitiveGameObject.AddComponent<MeshRenderer>();
+                currentMeshRenderer.material = new Material(Shader.Find("VR/SpatialMapping/Wireframe"));
+                currentMeshRenderer.material.color = Color.white;
+                
+                Mesh currentUnityMesh = new Mesh();
+                
+                accessor positionAccessor =  rootOfglTFFile.accessors[currentMeshPrimitive.attributes.POSITION];
+                accessor indicesAccessor = rootOfglTFFile.accessors[currentMeshPrimitive.indices]; //TODO: нормали + материалы
+
+                bufferView positionBufferView = rootOfglTFFile.bufferViews[positionAccessor.bufferView];
+                bufferView indicesBufferView = rootOfglTFFile.bufferViews[indicesAccessor.bufferView];
+
+                buffer positionBuffer = rootOfglTFFile.buffers[positionBufferView.buffer];
+                buffer indicesBuffer = rootOfglTFFile.buffers[indicesBufferView.buffer];
+
+                string positionBufferWithoutPrefix = positionBuffer.uri.Substring(glTFConvertionConsts.bufferPrefix.Length);
+                string indicesBufferWithoutPrefix = indicesBuffer.uri.Substring(glTFConvertionConsts.bufferPrefix.Length);
+
+                byte[] positionBytes = Convert.FromBase64String(positionBufferWithoutPrefix);
+                byte[] indicesBytes = Convert.FromBase64String(indicesBufferWithoutPrefix);
+
+                MemoryStream positionMemoryStream = new MemoryStream(positionBytes, positionBufferView.byteOffset, positionBufferView.byteLength);
+                MemoryStream indicesMemoryStream = new MemoryStream(indicesBytes,indicesBufferView.byteOffset,indicesBufferView.byteLength);
+                
+                Vector3[] vertices = new Vector3[positionAccessor.count];
+                
+                for (int vector3Index = 0; vector3Index < positionAccessor.count; vector3Index++)
+                {
+                    Vector3 currentVector3 = new Vector3();
+                    byte[] coordinateFloatBytes = new byte[12];
+                    positionMemoryStream.Read(coordinateFloatBytes, 0, 12);
+                    
+                    currentVector3.x = BitConverter.ToSingle(coordinateFloatBytes, 0);
+                    currentVector3.y = BitConverter.ToSingle(coordinateFloatBytes, 4);
+                    currentVector3.z = BitConverter.ToSingle(coordinateFloatBytes, 8);
+                    
+                    vertices[vector3Index] = currentVector3;
+                }
+                positionMemoryStream.Close();
+                currentUnityMesh.vertices = vertices;
+
+                int[] indices = new int[indicesAccessor.count];
+                
+                for (int indexIndex = 0; indexIndex < indicesAccessor.count; indexIndex++)
+                {
+                    byte[] triangleIndicesBytes = new byte[2];
+                    indicesMemoryStream.Read(triangleIndicesBytes, 0, 2);
+                    indices[indexIndex] = BitConverter.ToUInt16(triangleIndicesBytes,0);
+                }
+                
+                indicesMemoryStream.Close();
+                currentUnityMesh.triangles = indices;
+                currentMeshFilter.mesh = currentUnityMesh;
+            }
+        }
+        
+        
+    }
+}
---
+++ MarksEditor/Assets/Assets/Scripts/glTF/glTFExportClasses.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarksEditor.glTF
+{
+        
+    
+        /// <summary> Корневой класс glTF-файла/// </summary>
+        [Serializable]
+        public class root
+        {
+            public scene[] scenes;
+            public node[] nodes;
+            public mesh[] meshes;
+            public buffer[] buffers;
+            public bufferView[] bufferViews;
+            public accessor[] accessors;
+            public asset asset;
+        }
+    
+        
+        /// <summary> Класс сцены glTF-файла/// </summary>
+        [Serializable]
+        public class scene
+        {
+            public int[] nodes;
+        }
+    
+        /// <summary> Класс узла glTF-файла/// </summary>
+        [Serializable]
+        public class node
+        {
+            public int mesh;
+            public string name;
+            public float[] rotation;
+            public float[] scale;
+            public float[] translation;
+        }
+    
+        /// <summary> Класс меша glTF-файла/// </summary>
+        [Serializable]
+        public class mesh
+        {
+            public primitive[] primitives;
+            
+        }
+    
+        /// <summary> Класс примитивов меша glTF-файла/// </summary>
+        [Serializable]
+        public class primitive
+        {
+            public attribute attributes;
+            public int indices;
+    
+        }
+    
+        /// <summary> Класс атрибутов примитивов меша glTF-файла/// </summary>
+        [Serializable]
+        public class attribute
+        {
+            public int POSITION;
+    //        public int NORMAL;
+    //        public int TANGENT;
+    //        public int TEXCOORD_0;
+    //        public int TEXCOORD_1;
+    //        public int COLOR_0;
+    //        public int JOINTS_0;
+    //        public int WEIGHTS_0;
+        }
+    
+        /// <summary> Класс буфера с данными о меше glTF-файла/// </summary>
+        [Serializable]
+        public class buffer
+        {
+            public string uri = "data:application/octet-stream;base64,";
+            public int byteLength;
+        }
+    
+        /// <summary> Класс-описание частей буфера glTF-файла/// </summary>
+        [Serializable]
+        public class bufferView
+        {
+            public int buffer;
+            public int byteOffset;
+            public int byteLength;
+            public int target; //вершины 34962 индексы вершин 34963
+        }
+    
+        /// <summary> Класс-описание наследника glTF-файла/// </summary>
+        [Serializable]
+        public class accessor
+        {
+            public int bufferView;
+            public int byteOffset;
+            public int componentType;
+            public int count;
+            public string type;
+            public float[] max;
+            public float[] min;
+        }
+    
+        /// <summary> Класс описание ассета glTF-файла/// </summary>
+        [Serializable]
+        public class asset
+        {
+            public string version = "2.0";
+        }
+
+        public static class glTFConvertionConsts
+        {
+            public const string bufferPrefix  = "data:application/octet-stream;base64,";
+        }
+}
